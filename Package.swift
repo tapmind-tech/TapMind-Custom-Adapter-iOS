8628affbcf05c69bf91d8d92fca90074970718a9
Vivek Malani <vivek3693@gmail.com>
2026-02-09 14:40:10 +0530

    Updated release 1.0.2

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Google Mobile Ads official Swift Package
         .package(
             url: "https://github.com/kishantuvoc/TapMindSDK.git",
-            from: "1.0.0"
+            from: "1.1.2"
         )
     ],
     targets: [

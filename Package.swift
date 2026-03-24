86d116525b3b58e414726b6b7fc5b240531482b6
Kishan Italiya <kishan.italiya@tuvoc.com>
2026-03-24 14:25:57 +0530

    Update adapter with standard class naming and minor enhancements

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Google Mobile Ads official Swift Package
         .package(
             url: "https://github.com/kishantuvoc/TapMindSDK.git",
-            from: "1.1.5"
+            from: "1.1.6"
         )
     ],
     targets: [

5b18669a0d1498df8f541d9de2e09a4b5093b602
Kishan Italiya <kishan.italiya@tuvoc.com>
2026-08-18 15:24:00 +0530

    1.Update the new class names. 2.Implement multi-ad scenarios, timeout, refresh, and thread management. 3.Enable key request/response flow logs.

--- publish_pod.sh
+++ publish_pod.sh
@@ -7,25 +7,28 @@ VERSION=$(grep -E 's.version\s+=' "$PODSPEC_NAME" | cut -d '"' -f 2 | cut -d "'"
 echo "🚀 Starting publication process for $PODSPEC_NAME version $VERSION..."
 
 # 1. Lint the Podspec locally
-echo "🔍 Validating podspec..."
-if pod lib lint --allow-warnings; then
-    echo "✅ Lint passed!"
-else
-    echo "❌ Lint failed. Please fix errors before publishing."
-    exit 1
-fi
+#echo "🔍 Validating podspec..."
+#if pod lib lint --allow-warnings; then
+#    echo "✅ Lint passed!"
+#else
+#    echo "❌ Lint failed. Please fix errors before publishing."
+#    exit 1
+#fi
 
 # 2. Git Tagging
 echo "🏷️  Tagging version $VERSION in git..."
 git add .
 #git commit -m "Release version $VERSION"
-git commit -m "Removed logs"
+git commit -m "1.Update the new class names.
+2.Implement multi-ad scenarios, timeout, refresh, and thread management.
+3.Enable key request/response flow logs."
 git tag "$VERSION"
 git push origin main
 git push origin "$VERSION"
 
+echo "📤 Code pushed successfully..."
 # 3. Push to CocoaPods Trunk
-echo "📤 Pushing to CocoaPods Trunk..."
-pod trunk push "$PODSPEC_NAME" --allow-warnings
-
-echo "🎉 Successfully published $PODSPEC_NAME ($VERSION)!"
+#echo "📤 Pushing to CocoaPods Trunk..."
+#pod trunk push "$PODSPEC_NAME" --allow-warnings
+#
+#echo "🎉 Successfully published $PODSPEC_NAME ($VERSION)!"
